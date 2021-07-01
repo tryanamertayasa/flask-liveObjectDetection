7869b8d847235d8cf8e650dee72e146c46fd5852
tryanamertayasa <tryanamertayasa@gmail.com>
2021-07-01 14:23:10 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, Response
+import cv2
+import torch
+from PIL import Image
+from flask import Flask
+import io
+
+app = Flask(__name__)
+
+camera = cv2.VideoCapture(0)  # use 0 for web camera
+#  for cctv camera use rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' instead of camera
+# for local webcam use cv2.VideoCapture(0)
+
+def gen_frames():
+    model = torch.hub.load("ultralytics/yolov5",
+                           "yolov5s", force_reload=True)  # generate frame by frame from camera
+    while True:
+        # Capture frame-by-frame
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            # print(buffer)
+            frame = buffer.tobytes()
+            img = Image.open(io.BytesIO(frame))
+            results = model(img, size=640)
+            print(results.pandas().xyxy[0].to_json(orient="records"))
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+
+@app.route('/video_feed')
+def video_feed():
+    #Video streaming route. Put this in the src attribute of an img tag
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ yolov5/utils/flask_rest_api/test.py
@@ -0,0 +1,6 @@
+import base64
+imgstring= ""
+imgdata = base64.b64decode(imgstring)
+filename = 'some_image.jpg'  # I assume you have a way of picking unique filenames
+with open(filename, 'wb') as f:
+    f.write(imgdata)
